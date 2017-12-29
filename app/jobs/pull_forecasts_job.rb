95bf0a1023fa325d7c642afa70ddc3440a6899eb
Malte Hecht <malte.fisch@posteo.de>
2017-12-29 13:17:01 +0100

    PullForecast Job is no Active Job anymore since ActiveJobs dont work together with Resque Scheduler

--- app/jobs/pull_forecasts_job.rb
+++ app/jobs/pull_forecasts_job.rb
@@ -1,13 +1,13 @@
 # pull data for all forecasts and trigger notifications if necessary
-class PullForecastsJob < ApplicationJob
-  queue_as :default
-
+class PullForecastsJob
   # provider may be 'MagicSeaweed' etc.
-  def perform(provider = nil)
+  def self.perform(provider = nil)
     forecasts = provider.nil? ? Forecast.all : Forecast.where(provider: provider)
+    # pull each forecast and save the new data
     forecasts.each do |forecast|
       forecast.pull
       forecast.save
+      # update the related notifications
       forecast.notifications.each do |notification|
         notification.apply_rules_and_notify
         notification.save
--- docker/app/start_app.sh
+++ docker/app/start_app.sh
@@ -2,12 +2,16 @@
 
 # remove old pids
 rm tmp/pids/*
+
 # DB setup
 bundle exec rails db:create
 bundle exec rails db:migrate
-# start resque worker
-LOGGING=1 QUEUE=* bundle exec rake resque:work &
+
+# start resque workers
+COUNT=4 LOGGING=1 QUEUE=* bundle exec rake resque:workers &
+
 # start resque schedule
 bundle exec rake resque:scheduler &
+
 # starting server
 bundle exec rails s -p 3000 -b '0.0.0.0' # '127.0.0.1'
