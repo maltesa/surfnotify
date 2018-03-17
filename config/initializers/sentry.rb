efb3f312ee91d75a761d5e12a9c64b460d41035a
Malte Hecht <malte.fisch@posteo.de>
2018-03-17 12:54:31 +0100

    sentry url fix

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,3 @@
 Raven.configure do |config|
-  config.dsn = 'https://d7a6658f878c42a4bd4d417d25527209:44d1ae3430984a8291bccafe638dddbd@sentry.surfnotitfy.com/2'
+  config.dsn = 'https://d7a6658f878c42a4bd4d417d25527209:44d1ae3430984a8291bccafe638dddbd@sentry.surfnotify.com/2'
 end
