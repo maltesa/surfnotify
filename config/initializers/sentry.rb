265aa87f3eb1a6678bf8a8e59abd29785ef1cd7e
Malte Hecht <malte.fisch@posteo.de>
2018-07-09 00:51:45 +0200

    new entrypoint to separate containers, docker compose for production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,5 @@
-Raven.configure do |config|
-  config.dsn = 'https://d7a6658f878c42a4bd4d417d25527209:44d1ae3430984a8291bccafe638dddbd@sentry.surfnotify.com/2'
+if Rails.env.production?
+  Raven.configure do |config|
+    config.dsn = 'https://d7a6658f878c42a4bd4d417d25527209:44d1ae3430984a8291bccafe638dddbd@sentry.surfnotify.com/2'
+  end
 end
---
+++ docker/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# exit if command failes
+set -e
+
+case "$1" in
+  help)
+    echo "Usage: app|worker|scheduler|migrate|help"
+    ;;
+  app)
+    # starting server
+    rm /app/tmp/pids/* || true
+    exec puma -C config/puma.rb
+    ;;
+  worker)
+    # start resque workers 
+    bundle exec rake resque:prune_dead_workers
+    exec COUNT=3 LOGGING=1 QUEUE=* bundle exec rake resque:workers
+    ;;
+  scheduler)
+    # start resque scheduler
+    bundle exec rake resque:prune_dead_workers
+    exec bundle exec rake resque:scheduler
+    ;;
+  migrate)
+    bundle exec rails db:migrate
+    ;;
+  *)
+    exec "$@"
+    ;;
+esac
