f188962ca87054784a0b6460fe5fe6735fbc969f
Malte Hecht <malte.fisch@posteo.de>
2019-02-10 19:57:25 +0100

    fix ci #2

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
   Raven.configure do |config|
-    config.dsn = 'https://d7a6658f878c42a4bd4d417d25527209:44d1ae3430984a8291bccafe638dddbd@sentry.surfnotify.com/2'
+    config.dsn = ENV['SENTRY_DSN']
   end
 end
