97c2b1f109a8eea89a7433002ec7647cd19d87d4
Malte Hecht <malte.fisch@posteo.de>
2019-02-20 21:16:20 +0100

    fix entrypoint, fix sentry, commit sentry messages in jobs

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,13 @@ module Surfnotify
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    Raven.configure do |config|
+      config.async = lambda { |event| SentryJob.perform_later(event) }
+      config.dsn = ENV['SENTRY_DSN']
+      config.open_timeout = 10
+      config.environments = %w[production]
+      config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    end
   end
 end
--- config/initializers/sentry.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env.production?
-  Raven.configure do |config|
-    config.dsn = ENV['SENTRY_DSN']
-  end
-end
